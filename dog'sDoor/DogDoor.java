a1a0c74e843b483ad677221e8d83b434f46bd9bb
AIR <air.cse.pbn@gmail.com>
2015-07-11 23:31:52 +0600

    task to explore gathering requirements initiated

---
+++ dog'sDoor/DogDoor.java
@@ -0,0 +1,21 @@
+public class DogDoor{
+    private boolean open;
+    
+    public DogDoor(){
+        System.out.println(open);
+        this.open = false;
+        System.out.println(open);
+    }
+    
+    public void open(){
+        System.out.println("The dog door opens.");
+        open = true;
+    }
+    public void close(){
+        System.out.println("The dog door closes.");        
+        open = false;
+    }
+    public boolean isOpen(){
+        return open;
+    }
+}
