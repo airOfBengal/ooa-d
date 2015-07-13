114b42e1808152ad25009203eabec0ae2b938a88
AIR <air.cse.pbn@gmail.com>
2015-07-13 15:26:57 +0600

    DogDoor Updated to work correctly

--- dog'sDoor/Remote.java
+++ dog'sDoor/Remote.java
@@ -1,6 +1,3 @@
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Remote{
     private DogDoor door;
     
@@ -13,15 +10,7 @@ public class Remote{
         if(door.isOpen()){
             door.close();
         }else{
-            door.open();
-            
-            final Timer timer = new Timer();
-            timer.schedule(new TimerTask(){
-                public void run(){
-                    door.close();
-                    timer.cancel();
-                }
-            },5000);
+            door.open();           
         }
     }
 }
